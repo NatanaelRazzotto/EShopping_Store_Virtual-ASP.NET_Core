ec56f60cc51e519ec9656dc88de40e3c29e050dd
NATANAEL RAZZOTTO <natanael8447@outlook.com>
2023-02-19 11:08:46 -0300

    Add project files.

---
+++ EShopping.ProductApi/Context/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using EShopping.ProductApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EShopping.ProductApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        //FluentAPI
+    }
+}
---
+++ EShopping.ProductApi/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace EShopping.ProductApi.Models;
+public class Product
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public decimal Price { get; set; }
+    public string? Describe { get; set; }
+    public long Stock { get; set; }
+    public string? ImageUrl { get; set; }
+    public Category? Category { get; set; }
+    public int CategoryId { get; set; }
+
+}
